9209062de5a98ec9d7bb07b314c3b5f6ad7921b8
krissumhoe <krponla@gmail.com>
2023-06-20 22:47:44 +0800

    .

--- resources/views/temp/modal.blade.php
+++ resources/views/temp/modal.blade.php
@@ -1,9 +1,6 @@
-<!-- Trigger/Open The Modal -->
 <button id="AdminBtn">Crazy Modal</button>
 
-<!-- The Modal -->
 <div id="AdminModal" class="modal hidden fixed z-10 pt-28 top-0 left-0 w-full h-full overflow-auto bg-black bg-black/[0.4]">
-    <!-- Modal content -->
     <div class="bg-dirty-white m-auto p-5 border-1 rounded w-5/6">
         <span class="close font-deep-green float-right text-xl font-bold hover:bg-black hover:cursor-pointer">&times;</span>
         <p>Fart</p>
@@ -16,26 +13,24 @@
 </div>
 
 <script>
-// Get the modal
+
 var modal = document.getElementById("AdminModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("AdminBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// Open Modal
 btn.onclick = function() {
 modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// Close Modal (using the X button)
 span.onclick = function() {
 modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Close Modal (clicking anywhere else outside the Modal)
 window.onclick = function(event) {
 if (event.target == modal) {
     modal.style.display = "none";
