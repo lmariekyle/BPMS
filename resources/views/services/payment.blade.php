5ba8e8bad758cde5f62f28595ff136497d86dc6c
lourdeskyle <lourdesmkc@gmail.com>
2023-10-11 14:27:33 -0700

    idkk

---
+++ resources/views/services/payment.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Resident') }}
+        </h2>
+    </x-slot>
+
+    <div class="flex flex-col">
+        
+    </div>
+</x-app-layout>
