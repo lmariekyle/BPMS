d8ef15680f4b44a37f03a7950ac34346d397427a
Flas13 <76106600+Flas13@users.noreply.github.com>
2024-02-22 23:03:41 +0800

    Login Module
    Initial Changes in Mobile Login Backend

--- app/Http/Controllers/Auth/AuthenticationAPIController.php
+++ app/Http/Controllers/Auth/AuthenticationAPIController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Sitio;
+use App\Models\Resident;
 use App\Models\Statistics;
 use DB;
 use Carbon\Carbon;
@@ -63,6 +64,44 @@ class AuthenticationAPIController extends Controller
             return $response = ['success' => true];
         }else{
             return $response = ['success' => false];
+        }
+    }
+
+    public function sanctumLogin(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if($user && Hash::check($request->password, $user->password)){
+            $token = $user->createToken($user->residentID)->plainTextToken;
+            $resident = Resident::where('id', $user->residentID)->first();
+            $user->username = $resident->firstName . ' ' . $resident->lastName;
+            if($user->userLevel == "Barangay Health Worker"){
+                $sitio = Sitio::where('id', $user->assignedSitioID)->first();
+                $user->assignedSitio  = $sitio->sitioName;
+
+                $quarter = 1;
+                $statistic = null;
+                $year =  Statistics::max('year');
+                $statisticsYear = Statistics::where('year', $year)->get();
+                foreach($statisticsYear as $statistics){
+                    if($statistics->quarter > $quarter){
+                        $statistic = $statistics;
+                        $quarter = $statistic->quarter;
+                    }
+                }
+                $response = ['user' => $user, 'statistics' => $statistic,'token' => $token, 'success' => true];
+            }else{
+                $response = ['user' => $user, 'token' => $token, 'success' => true];
+            }
+            return response()->json($response, 200);
+        }else{
+            $response = ['message' => 'Incorrect email or password', 'success' => false];
+            return response()->json($response, 200);
         }
     }
 }
